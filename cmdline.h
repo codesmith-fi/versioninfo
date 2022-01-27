1db86f93797655997d4ff3d069fb5e2ad34e2c5a
Erno Pakarinen <codesmith.fi@gmail.com>
2022-01-27 10:46:27 +0200

    Added help text support for commandlineargs

--- cmdline.h
+++ cmdline.h
@@ -8,26 +8,38 @@ namespace codesmith {
 	namespace util {
 		class CommandLineOption {
 		public:
-			CommandLineOption(const std::string keyword, bool required = false)
-				: m_keyword(keyword), m_required(required) { };
+			CommandLineOption(const std::string keyword, const std::string help, bool required = false)
+				: m_keyword(keyword), m_helpText(help), m_required(required) { };
 			virtual ~CommandLineOption() { };
 
 			const std::string& keyword() const { return m_keyword; };
+			const std::string& helpText() const { return m_helpText; };
 		private:
 			std::string m_keyword;
+			std::string m_helpText;
 			bool m_required;
 		};
 
 		class CommandLineArgs {
 		public:
 			CommandLineArgs(int argc, char** argv) {
-				for(auto i = 0; i < argc; ++i) {
+				for (auto i = 0; i < argc; ++i) {
 					std::string opt(argv[i]);
 					m_args.emplace_back(opt);
 				}
 			}
 
 			virtual ~CommandLineArgs() { };
+
+			void addOption(const CommandLineOption& option) {
+				m_options.emplace_back(option);
+			}
+
+			void showHelp() const {
+				for (const CommandLineOption opt : m_options) {
+					std::cout << "  " << opt.keyword() << "\t\t" << opt.helpText() << "\n";
+				}
+			}
 
 			bool optionExists(const CommandLineOption& option) const {
 				bool result = false;
@@ -42,6 +54,7 @@ namespace codesmith {
 			}
 		private:
 			std::vector<std::string> m_args;
+			std::vector<CommandLineOption> m_options;
 		};
 	} // namespace util
 } // namespace codesmith
--- versioninfo.cpp
+++ versioninfo.cpp
@@ -10,9 +10,20 @@ int main(int argc, char** argv)
 {
     codesmith::types::Version ver;
     codesmith::util::CommandLineArgs args(argc, argv);
-    codesmith::util::CommandLineOption optionInit("-i"); // init version
+    codesmith::util::CommandLineOption optionInit("-i",
+        "Initialize version information with given details"); // init version
+    codesmith::util::CommandLineOption optionHelp("-h",
+        "Show this help"); // help
 
-    if(args.optionExists(optionInit)) {
+    args.addOption(optionHelp);
+    args.addOption(optionInit);
+
+    if (args.optionExists(optionHelp)) {
+        std::cout << "VersionInfo v0.1.0 by Erno Pakarinen (codesmith.fi@gmail.com)\n";
+        std::cout << "Following command line options can be used:\n";
+        args.showHelp();
+        return 0;
+    } else if(args.optionExists(optionInit)) {
         std::cout << "Initializing version";
         return 0;
     }
